b27537d5542cab34c79f0a7b570b2508eb5e4337
mohanprasaths <mohanprasath@live.com>
2017-06-20 16:35:29 +0530

    square as function conponent

--- src/index.js
+++ src/index.js
@@ -5,18 +5,28 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={()=>alert("hello")}>
-        {/* TODO */}
+      <button className="square" onClick={()=> this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor(){
+    super();
+    this.state = {
+      squares : Array(9).fill(null)
+    }
   }
-
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+  }
+  handleClick(i){
+    const squares = this.state.squares.slice()
+    squares[i] = "X";
+    this.setState({squares : squares})
+  }
   render() {
     const status = 'Next player: X';
 
